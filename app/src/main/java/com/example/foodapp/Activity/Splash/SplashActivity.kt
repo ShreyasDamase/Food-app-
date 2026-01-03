cbc7fc3d7a4b24891f26b1ca8ab173c9c1cd5ead
ShreyasDamase <shreyasdamase@gmail.com>
2026-01-03 20:30:20 +0530

    first commit
    # Conflicts:
    #	.gitignore

---
+++ app/src/main/java/com/example/foodapp/Activity/Splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.foodapp.Activity.Splash
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.foodapp.R
+
+class SplashActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+   
+    }
+}
